e2ef234f9373d33e593e369d9b4c07e805401715
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-10-01 11:56:37 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MenuButtonComponent } from './components/menu-button/menu-button.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PuntoSeguroComponent } from './components/punto-seguro/punto-seguro.component';
+import { PublicServiceComponent } from './components/public-service/public-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MenuButtonComponent,
+    ProfileComponent,
+    PuntoSeguroComponent,
+    PublicServiceComponent
   ],
   imports: [
     BrowserModule
