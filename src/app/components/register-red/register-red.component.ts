053eb02cf075daabeb98f7c419905c763218024c
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-10-01 12:16:53 -0300

    new component

---
+++ src/app/components/register-red/register-red.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-red',
+  templateUrl: './register-red.component.html',
+  styleUrls: ['./register-red.component.css']
+})
+export class RegisterRedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
