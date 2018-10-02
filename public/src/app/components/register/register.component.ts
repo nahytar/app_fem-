0b9989dc41770d706828ca9e3ae0e9df9dfb8905
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-10-01 23:09:59 -0300

    registro

--- public/src/app/components/register/register.component.ts
+++ public/src/app/components/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { RutValidator} from 'ng2-rut';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +13,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, public snackBar: MatSnackBar, private router: Router, public rv: RutValidator) { 
+    this.createRegisterForm();
+  }
 
   ngOnInit() {
   }
 
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      // rut: ['', Validators.compose([Validators.required, rv])],
+      emailRegister: ['', Validators.compose([Validators.required, Validators.email])],
+      pass: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      name: ['', Validators.compose([Validators.required])],
+      phone: ['', Validators.compose([Validators.required, Validators.minLength(9)])],
+      femele: ['', Validators.compose([Validators.requiredTrue])]
+    });
+  }
+
+  onRegister() {
+    this.authService.signUp(this.registerForm.value.emailRegister, this.registerForm.value.pass)
+    .then(() => {
+      this.router.navigate(['/home']);
+    })
+    .catch(() => {
+      this.snackBar.open('Error en tu Registro, ¡Intentemoslo otra vez!', null , {
+        duration: 3000,
+      });
+    });
+  }
 }
--- public/src/app/service/auth.service.ts
+++ public/src/app/service/auth.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument,  AngularFirestoreCollection } from '@angular/fire/firestore';
+import * as firebase from 'firebase'; 
+import { Observable } from 'rxjs';
+
+interface User {
+  uid: string;
+  email: string;
+  photoURL?: any;
+  displayName?: string;
+  phone: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user: Observable<firebase.User>;
+  usersCollection: AngularFirestoreCollection<any>;
 
-  constructor() { }
+  constructor(private firebaseAuth: AngularFireAuth, private afAuth: AngularFireAuth,
+    private afs: AngularFirestore) { 
+      this.user = firebaseAuth.authState;
+      this.usersCollection = afs.collection<any>('info');
+    }    
+  
+  signUp(email: string, password: string) {
+    return this.firebaseAuth
+    .auth
+    .createUserWithEmailAndPassword(email, password);
+  }    
+
+  login(email: string, password: string) {
+    return this.firebaseAuth
+    .auth
+    .signInWithEmailAndPassword(email, password);
+  }
+  logOut() {
+    return this.firebaseAuth.auth
+    .signOut();
+  }
+
+  uploadUserToFirestore() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        console.log(user);
+        const data: User = {
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName, 
+          photoURL: user.photoURL,
+          phone: user.phoneNumber
+      };
+      return this.afs.collection(`users`).doc(`${user.uid}`).set(data);
+    }
+    });
+  }
+
+  private updateUserData(user: User) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(
+      `users/${user.uid}`
+    );
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      phone: user.phone
+    };
+    return userRef.set(data);
+  }    
 }
