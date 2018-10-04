bdd9b85d7168a17be27f5ad8a00ecc7b52822f31
nahytar <nahytar@gmail.com>
2018-10-04 10:25:21 -0300

    se imprime geolocalizacion

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -67,7 +67,7 @@ export class MapComponent implements OnInit {
         center: { lat: this.lat, lng: this.lng }
       }
     );
-    
+    this.displayUserMarker()
     let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
     this.ui = H.ui.UI.createDefault(this.map, defaultLayers);
   };
@@ -96,5 +96,16 @@ export class MapComponent implements OnInit {
     this.map.addObject(marker);
   }
 
-
+  displayUserMarker() {
+    let svgMarkup = '<svg aria-hidden="true" data-prefix="fas" width="30" height="30" data-icon="map-marker-alt" class="svg-inline--fa fa-map-marker-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>';
+    let svgIcon = new H.map.Icon(svgMarkup),
+    coords = {
+      lat: this.lat,
+      lng: this.lng
+    },
+    marker = new H.map.Marker(coords, {
+      icon: svgIcon
+    });
+  this.map.addObject(marker);
+  }
 }
