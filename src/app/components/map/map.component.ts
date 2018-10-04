485ddaac8b2b1fd98d8024811161a40bdb25df46
nahytar <nahytar@gmail.com>
2018-10-04 13:05:56 -0300

    se corrige mapa

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 
+
 declare var H: any;
 
 
@@ -11,6 +12,7 @@ declare var H: any;
 export class MapComponent implements OnInit {
   @ViewChild("map")
   public mapElement: ElementRef;
+  search: any;
 
   @Input()
   public appId: any;
@@ -18,11 +20,11 @@ export class MapComponent implements OnInit {
   @Input()
   public appCode: any;
 
-  // @Input()
-  // public lat: any;
+  @Input()
+  public lat: any;
 
-  // @Input()
-  // public lng: any;
+  @Input()
+  public lng: any;
 
   @Input()
   public width: any;
@@ -34,30 +36,29 @@ export class MapComponent implements OnInit {
   private map: any;
 
   private ui: any;
-  private search: any;
   public directions: any;
-  lat: any;
-  lng: any;
+  // lat: any;
+  // lng: any;
 
   public constructor() { }
 
   public ngOnInit() {
-    
     this.platform = new H.service.Platform({
       "app_id": this.appId,
       "app_code": this.appCode
     });
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position)=>{
+      navigator.geolocation.getCurrentPosition((position) => {
         this.lat = position.coords.latitude;
         this.lng = position.coords.longitude;
         this.showPosition();
       });
-    this.search = new H.places.Search(this.platform.getPlacesService());
-    this.directions = [];
+      this.search = new H.places.Search(this.platform.getPlacesService());
+      this.directions = [];
+    }
   }
-  }
-  showPosition(){
+
+  showPosition() {
     let defaultLayers = this.platform.createDefaultLayers();
     this.map = new H.Map(
       this.mapElement.nativeElement,
@@ -99,13 +100,13 @@ export class MapComponent implements OnInit {
   displayUserMarker() {
     let svgMarkup = '<svg aria-hidden="true" data-prefix="fas" width="30" height="30" data-icon="map-marker-alt" class="svg-inline--fa fa-map-marker-alt fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>';
     let svgIcon = new H.map.Icon(svgMarkup),
-    coords = {
-      lat: this.lat,
-      lng: this.lng
-    },
-    marker = new H.map.Marker(coords, {
-      icon: svgIcon
-    });
-  this.map.addObject(marker);
+      coords = {
+        lat: this.lat,
+        lng: this.lng
+      },
+      marker = new H.map.Marker(coords, {
+        icon: svgIcon
+      });
+    this.map.addObject(marker);
   }
 }
